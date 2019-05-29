859a34cb66081c353bffd77438921dbbc5fa3ca5
Nishchint Dhawan <nishchintdhawan@MacBook-Air.hitronhub.home>
2019-05-28 21:02:05 -0700

    Nishchint - added the educationform in the editing mode

--- assets/js/view_profile.js
+++ assets/js/view_profile.js
@@ -36,11 +36,11 @@ $(document).ready(function() {
     });
 });
 
-// Function to view the introduction 
+// Function to view the introduction
 function viewIntroduction(introductionJSON){
     // Add the profile picture
     $('#profile-picture').attr("src", introductionJSON.profileImg);
-    // Adding name, headline and personal description 
+    // Adding name, headline and personal description
     $('#name').html(`${introductionJSON.firstName} ${introductionJSON.lastName}`);
     $('#personal-headline').html(`${introductionJSON.headline}`);
 
@@ -54,11 +54,11 @@ function viewNavbarDropMenu(introductionJSON){
     $('#nameDropMenu').html(`${introductionJSON.firstName} ${introductionJSON.lastName}`);
 }
 
-// Function to view the Education section 
+// Function to view the Education section
 function viewEducation(educationJSON){
-    // getting the total number of education 
+    // getting the total number of education
     var totalNumberOfEducation = Object.keys(educationJSON).length;
-    // Template string which will store the html of all the education 
+    // Template string which will store the html of all the education
     var eduHtml = ``;
 
     // for each education, add the education to the string
@@ -85,7 +85,7 @@ function viewEducation(educationJSON){
     $("#education-fields").html(eduHtml);
 }
 
-// Function to view the Work Experience section 
+// Function to view the Work Experience section
 function viewWorkExperience(workExperienceJSON){
 
     // If work experience is not empty
@@ -126,14 +126,14 @@ function viewWorkExperience(workExperienceJSON){
         // Appending the template literal string to the work experience section (*** .append() also works in the same way)
         $("#work-fields").html(html);
     }
-    // If no work experience then remove the work experience section 
+    // If no work experience then remove the work experience section
     else {
         $("#work-fields").parent().parent().parent().remove();
     }
 }
 
 
-// Function to view Skill section 
+// Function to view Skill section
 function viewSkills(skillJSON){
 
     if(skillJSON != null && skillJSON != {} && skillJSON != undefined){
@@ -150,11 +150,11 @@ function viewSkills(skillJSON){
             var column2 = Math.floor(eachColumn);
         }
 
-        //adding skills 
+        //adding skills
         var skillHtml = ``;
         var num = 1;
         skillHtml = `${skillHtml}<div class="accordion row" id="accordionExample">
-                                    <div class ="col-lg-6 container-fluid">`;                              
+                                    <div class ="col-lg-6 container-fluid">`;
         for(i = 0; i < column1; i++){
             var child = "skill" + num;
             skillHtml = `${skillHtml}<div class="card row" id="skill-${num}">
@@ -189,8 +189,8 @@ function viewSkills(skillJSON){
             num++;
         }
         skillHtml = `${skillHtml}</div></div>`;
-        
-        // Appending 
+
+        // Appending
         $("#skills-section").html(skillHtml);
     }
     // Else remove the skill section
@@ -201,7 +201,7 @@ function viewSkills(skillJSON){
 
 
 // function to view the profile photos
-function viewPhotos(photoJSON) { 
+function viewPhotos(photoJSON) {
     // if there is a photoJSON or if its not empty or undefined
     if(photoJSON != null && photoJSON != {} && photoJSON != undefined){
         //get the total number of photos
@@ -220,7 +220,7 @@ function viewPhotos(photoJSON) {
                 // Get the URL and description of the image
                 var urlSrc = image.url;
                 var imgDescription = image.description;
-                
+
                 //TODO change image to iframe to improve UX
                 photoHtml = `${photoHtml}<div class="card col-lg-3 border-primary">
                                             <div class="pic-content">
@@ -244,24 +244,24 @@ function viewPhotos(photoJSON) {
                     // adding a new row when the current one is filled
                     if(imgIndex < totalNumberOfPhotos){
                         photoHtml = `${photoHtml}<br> <div class="row ml-2">`;
-                    }                        
-                }                          
+                    }
+                }
             })
         }
-        else { 
+        else {
             photoHtml = `<h3>No Photos.</h3>`;
         }
-    // Appending the template literal string to the photos section 
+    // Appending the template literal string to the photos section
     $("#photos-section").html(photoHtml);
     }
-    // If there are no photos 
+    // If there are no photos
     else {
         $("#photos-section").html(`<h3>No Photos.</h3>`);
     }
-}   
+}
 
 // Function to view the Image in a modal (it gives the source of image to the modal to view the picture)
-function viewImageModal(elementRef){    
+function viewImageModal(elementRef){
     // Get the url (image source) of the image
     var imgSrc = $($(elementRef).parent().parent().children()[0]).attr('src')
     // Change the modal's image source to the above
@@ -278,7 +278,7 @@ function viewImgDescModal(elementRef){
     var imgSrc = $($(elementRef).parent().parent().children()[0]).attr('src')
     // Change the modal's image source to the above
     $("#ModalDispDescImg").attr('src',imgSrc);
-    // Adding the description to html 
+    // Adding the description to html
     $("#descContainer").html($(elementRef).attr('data').replace(/\n/g, "</br>"));
 
     // Adding properties to the icons to view the modal
@@ -288,18 +288,18 @@ function viewImgDescModal(elementRef){
 
 }
 
-// Function to view the videos section 
+// Function to view the videos section
 function viewVideos(videoJSON){
     // if there is a videoJSON or if its not empty or undefined
     if(videoJSON != null && videoJSON != {} && videoJSON != undefined){
-        //total number of videos 
+        //total number of videos
         var totalNumberOfVideos = Object.keys(videoJSON).length;
         // template string to add the videos
         var videoHtml = ``;
         // If there are videos present in the videoJSON
         if(totalNumberOfVideos){
             //html content to be appended in html file
-            videoHtml = `<div class="row ml-2">`; 
+            videoHtml = `<div class="row ml-2">`;
 
             $.each(videoJSON, function(videoKeyName, video){
                 var videoIndex = parseInt(videoKeyName.replace('video',''));
@@ -320,35 +320,36 @@ function viewVideos(videoJSON){
 
                     if(videoIndex < totalNumberOfVideos){
                         videoHtml = `${videoHtml}<br> <div class="row ml-2">`;
-                    }                        
-                }    
+                    }
+                }
             })
         }
-        // If there are no videos uploaded 
+        // If there are no videos uploaded
         else{
             videoHtml = `<h3>No Videos.</h3>`
         }
-        // Appending the template literal to the videos section 
+        // Appending the template literal to the videos section
         $("#videos-section").html(videoHtml);
     }
     // if there are no videos
-    else { 
+    else {
         $("#videos-section").append(`<h3>No Videos.</h3>`);
     }
 }
 
-// When the edit button is clicked 
+// When the edit button is clicked
 function editPage(){
 
     //TODO add button to send changes
 
     //TODO replace the edit button with cancel button
-    
+
     //TODO add more edit functions
+    // console.log("Editing the page").
     editAboutMe()
     editEducation()
     editWorkExperience()
-    
+
 
 }
 
@@ -357,11 +358,94 @@ function editAboutMe() {
 }
 
 function editEducation() {
-   
-} 
+
+
+    educationJSON = profileJSOn.education;
+  // getting the total number of education
+  var totalNumberOfEducation = Object.keys(educationJSON).length;
+  // Template string which will store the html of all the education
+  var eduHtml = ``;
+
+
+  // for each education, add the education to the string
+  $.each(educationJSON, function(i, education){
+
+    var courseField = education.courseField;
+    var dateOfGraduation = education.dateOfGraduation;
+    var degreeType = education.degreeType;
+    var school = education.school;
+
+    var educationNumber = i.replace("education", ""); //gets the education number from the database using the educationJSON.
+
+      eduHtml = `${eduHtml}
+                    <div id = "extra-education-${educationNumber}">
+                    <br>
+                    <div id = "education-${educationNumber}">
+                      <h6 class="heading mb-4 text-center text-primary">Education ${educationNumber}</h6>
+                    <div class="row">
+                      <div class="col-lg-12">
+                          <div class="row">
+                              <div class="col-lg-5">
+                                 <div class = "form-group">
+                                    <label class ="form-control-label">Course Field</label>
+                                      <br>
+                                    <input type = "textarea" class="form-control form-control-alternative extra-education-${educationNumber}" placeholder="courseField" value = "${courseField}" >
+                                 </div>
+
+                              </div>
+                              <div class="col-lg-5">
+                                  <div class = "form-group">
+                                    <label class = "form-control-label">School/University</label>
+                                      <br>
+                                    <input type = "textarea" class = "form-control form-control-alternative extra-education-${educationNumber}" placeholder="school" value = "${school}" >
+                                  </div>
+                              </div>
+
+                              <div class="col-lg-5">
+                                  <div class = "form-group">
+                                    <label class = "form-control-label">Degree Type</label>
+                                      <br>
+                                    <input type = "textarea" class = "form-control form-control-alternative extra-education-${educationNumber}" placeholder="degreeType" value = "${degreeType}" >
+                                  </div>
+                              </div>
+
+                              <div class="col-lg-5">
+                                  <div class = "form-group">
+                                    <label class = "form-control-label">Date of Graduation</label>
+                                      <br>
+                                    <input type = "textarea" class = "form-control form-control-alternative extra-education-${educationNumber}" placeholder="dateOfGraduation" value = "${dateOfGraduation}" >
+                                  </div>
+                              </div>
+
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+                    </div>`;
+
+
+
+       eduHtml = `${eduHtml} <div id="add-extra-work-experience-buttons">
+                  <button class="btn btn-icon btn-2 btn-primary" type="button" onclick="addExtraWorkExperience()" >
+
+                    <span class="btn-inner--icon"><i class="ni ni-fat-add"></i></span>
+                  </button>
+
+                  <button class="btn btn-icon btn-2 btn-danger" type="button" id="remove-extra-work-experience" onclick="removeExtraWorkExperience()">
+                    <span class="btn-inner--icon"><i class="ni ni-fat-delete"></i></span>
+                  </button>
+                  </div>`;
+      // there's a line after every education to separate two sections, so we add it here
+      if(i != totalNumberOfEducation){
+          eduHtml = `${eduHtml}<hr style="margin-top:2%; margin-bottom: 4%;"></hr>`;
+      }
+  });
+  // Appending the template literal string to the education section (*** .append() also works in the same way)
+  $("#education-fields").html(eduHtml);
+}
 
 function editWorkExperience() {
-    
+
     //Make a DOM ready to take new template
     $("#work-fields").empty();
 
@@ -427,8 +511,8 @@ function editWorkExperience() {
                                     <div class="row">
                                     <div class="col-lg-12">
                                         <div class="form-group">
-                                        <label class="form-control-label\">Job Discription</label>
-                                        <textarea rows="8" class="form-control form-control-alternative extra-work-experience-${workExperienceNumber}" placeholder="Discribe your work in few words ...">${workDescription}</textarea>
+                                        <label class="form-control-label\">Job Description</label>
+                                        <textarea rows="8" class="form-control form-control-alternative extra-work-experience-${workExperienceNumber}" placeholder="Describe your work in few words ...">${workDescription}</textarea>
                                         </div>
                                     </div>
                                     </div>
@@ -450,7 +534,7 @@ function editWorkExperience() {
 
     // Appending the template literal string to the work experience section (*** .append() also works in the same way)
     $("#work-fields").html(html);
-} 
+}
 
 
 //This function is called when "+" button is pressed.
@@ -517,7 +601,7 @@ function addExtraWorkExperience() {
     }
 }
 
-//this function is called when remove extra work info button is pressed 
+//this function is called when remove extra work info button is pressed
 function removeExtraWorkExperience() {
     //check total number of extra education info forms and making id name to remove last form
     var totalNumberOfWorkExperienceRecord = $("#work-experience-container").children().length;
